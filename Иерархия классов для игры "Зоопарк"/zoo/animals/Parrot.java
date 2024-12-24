ae7a731588617eabbc988afd3c558634ecda37e2
Nikita Lysansky <158537164+nikitalysansky@users.noreply.github.com>
2024-12-24 18:53:55 +0300

    Update Parrot.java

--- Иерархия классов для игры "Зоопарк"/zoo/animals/Parrot.java
+++ Иерархия классов для игры "Зоопарк"/zoo/animals/Parrot.java
@@ -1,1 +1,17 @@
+package zoo.animals;
 
+public class Parrot extends AbstractAnimal {
+    public Parrot(String name) {
+        super(name);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " ест фрукты.");
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(getName() + " говорит: Привет!");
+    }
+}
