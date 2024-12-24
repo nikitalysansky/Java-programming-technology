f8f57d92d549f00174d2c8226b7ec9977771740a
Nikita Lysansky <158537164+nikitalysansky@users.noreply.github.com>
2024-12-24 18:54:54 +0300

    Update Zookeeper.java

--- Иерархия классов для игры "Зоопарк"/zoo/staff/Zookeeper.java
+++ Иерархия классов для игры "Зоопарк"/zoo/staff/Zookeeper.java
@@ -1,1 +1,19 @@
+package zoo.staff;
 
+import zoo.animals.Animal;
+
+public class Zookeeper extends Staff {
+    public Zookeeper(String name) {
+        super(name);
+    }
+
+    @Override
+    public void performDuties() {
+        System.out.println(getName() + " ухаживает за животными.");
+    }
+
+    public void feedAnimal(Animal animal) {
+        System.out.println(getName() + " кормит " + animal.getName() + ".");
+        animal.eat();
+    }
+}
